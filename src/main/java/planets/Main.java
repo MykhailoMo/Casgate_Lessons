c05496844dc37e11b487444cc5ff7841c84566ac
MykhailoMo <qatest.mikhail@gmail.com>
2023-11-06 15:43:54 +0100

    Planets

--- src/main/java/planets/Main.java
+++ src/main/java/planets/Main.java
@@ -1,6 +1,8 @@
 package planets;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static planets.PlanetsEnum.*;
@@ -8,15 +10,21 @@ import static planets.Utils.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<PlanetsEnum> planets = new ArrayList<>();
-        planets.add(MERCURY);
-        planets.add(VENUS);
-        planets.add(EARTH);
-        planets.add(MARS);
-        planets.add(JUPITER);
-        planets.add(SATURN);
-        planets.add(URANUS);
-        planets.add(NEPTUNE);
+//        List<PlanetsEnum> planets = new ArrayList<>(); //1
+//        planets.add(MERCURY);
+//        planets.add(VENUS);
+//        planets.add(EARTH);
+//        planets.add(MARS);
+//        planets.add(JUPITER);
+//        planets.add(SATURN);
+//        planets.add(URANUS);
+//        planets.add(NEPTUNE);
+//        List<PlanetsEnum> planets = List.of(PlanetsEnum.values());  //2
+        List<PlanetsEnum> planets = Arrays.asList(PlanetsEnum.values());  //3
+//        Arrays.stream(PlanetsEnum.values()).toList();  //4
+//        List<PlanetsEnum> planets = new ArrayList<>();
+//        Collections.addAll(planets, PlanetsEnum.values());  //5
+
         for (PlanetsEnum planet: planets) {
             System.out.println("Time from Sun shutdown to astronomers is: " + timeToEarth(planet) + "s to see the " + planet.getName());
         }
